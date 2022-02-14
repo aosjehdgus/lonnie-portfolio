7f1843d5e7ebeb11b701cb0912c46e24d1009b5c
aosjehdgus <kdhsea@gmail.com>
2022-02-14 12:02:03 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div className="container">CRA 없이 개발 환경 세팅하기</div>;
+  return <div className="container">CRA 없이 개발 환경 세팅하기 TEST</div>;
 };
 
 export default App;
