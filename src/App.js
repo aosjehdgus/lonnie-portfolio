47e95f8c99022fb2303dfd1c034a6ce64e1a3a16
aosjehdgus <kdhsea@gmail.com>
2022-02-15 14:42:16 +0900

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import Entry from './pages/Entry';
+import Home from './pages/Home';
+import About from './pages/About';
+
 const App = () => {
-  return <div className="container">CRA 없이 개발 환경 세팅하기 TEST</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Entry />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 };
 
 export default App;
