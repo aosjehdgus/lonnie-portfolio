f36918119b0e632598d8709d326eb3b1b3b4836f
aosjehdgus <kdhsea@gmail.com>
2022-02-16 16:54:16 +0900

    Add emotion global style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+const GlobalStyle = css`
+  @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+  * {
+    font-family: 'Varela Round', sans-serif;
+  }
+`;
+export default GlobalStyle;
