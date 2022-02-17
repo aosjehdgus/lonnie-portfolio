ae06c82afe81ca660f609a38424037b601a131d7
aosjehdgus <kdhsea@gmail.com>
2022-02-17 18:35:31 +0900

    Apply navigation components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
+import {
+  ENTRY_PATH,
+  HOME_PATH,
+  PROFILE_PATH,
+  PROJECT_PATH,
+  SKILL_PATH,
+  TECH_PATH,
+} from './configs/AppConfig';
+
+import HorizontalNav from './components/nav/horizontal-nav';
 import Entry from './pages/Entry';
 import Home from './pages/Home';
-import About from './pages/About';
+import Profile from './pages/Profile';
+import Project from './pages/Project';
+import Skill from './pages/Skill';
+import Tech from './pages/Tech';
 
 const App = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
-    <Routes>
-      <Route path="/" element={<Entry />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      {pathname === ENTRY_PATH ? null : <HorizontalNav />}
+      <Routes>
+        <Route path={ENTRY_PATH} element={<Entry />} />
+        <Route path={HOME_PATH} element={<Home />} />
+        <Route path={PROFILE_PATH} element={<Profile />} />
+        <Route path={PROJECT_PATH} element={<Project />} />
+        <Route path={SKILL_PATH} element={<Skill />} />
+        <Route path={TECH_PATH} element={<Tech />} />
+      </Routes>
+    </>
   );
 };
 
