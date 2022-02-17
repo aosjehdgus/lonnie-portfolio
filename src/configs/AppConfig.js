d13526c49ed898a180e7deb153ff177b2bdb34de
aosjehdgus <kdhsea@gmail.com>
2022-02-17 18:32:09 +0900

    Add entry, skill, tech path to app config

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,4 +1,6 @@
+export const ENTRY_PATH = '/';
 export const HOME_PATH = '/home';
 export const PROFILE_PATH = '/profile';
 export const PROJECT_PATH = '/project';
-export const SKILL_PATH = './skill';
+export const SKILL_PATH = '/skill';
+export const TECH_PATH = '/tech';
