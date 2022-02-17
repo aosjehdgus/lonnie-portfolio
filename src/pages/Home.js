c1dbfc706a63fd2b990d56247160d654ecbf4d4e
aosjehdgus <kdhsea@gmail.com>
2022-02-17 18:31:42 +0900

    Add page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <h1>홈</h1>
+      <h1>Home</h1>
+      <p>Home 페이지</p>
     </div>
   );
 };
---
+++ src/pages/Tech.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tech = () => {
+  return (
+    <div>
+      <h1>Tech</h1>
+      <p>Tech 페이지</p>
+    </div>
+  );
+};
+
+export default Tech;
