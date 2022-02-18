25bcf56a31cf8a2c53a95fa16e72ce8ea2def519
aosjehdgus <kdhsea@gmail.com>
2022-02-18 12:37:14 +0900

    Transfer entry component to home

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,5 +1,4 @@
-export const ENTRY_PATH = '/';
-export const HOME_PATH = '/home';
+export const HOME_PATH = '/';
 export const PROFILE_PATH = '/profile';
 export const PROJECT_PATH = '/project';
 export const SKILL_PATH = '/skill';
