f4d8340b0c5460d637542b9cd47bfd378861d0e4
aosjehdgus <kdhsea@gmail.com>
2022-02-21 19:41:47 +0900

    Change toggle css and need to check again

--- src/components/nav/horizontal-nav/NavToggle.js
+++ src/components/nav/horizontal-nav/NavToggle.js
@@ -23,8 +23,9 @@ const navToggle = css`
   }
   //배경(Dark mode)
   input:checked + label {
-    background: #0e0c32;
-    border: 5px solid #fffad8;
+    background: rgba(0, 51, 102, 1);
+    box-shadow: inset -3px -3px 7px rgba(0, 38, 77, 1),
+      inset 3px 3px 5px rgba(0, 79, 158, 1);
   }
   // 공(Dark mode)
   input:checked + label:after {
@@ -32,22 +33,28 @@ const navToggle = css`
     justify-content: center;
     align-items: center;
     font-weight: 700;
-    background: #becec3;
-    content: 'Dark';
-    left: calc(100% - 52.5px);
+    background: rgba(255, 222, 56, 1);
+    box-shadow: inset -3px -3px 7px rgba(255, 224, 66, 1),
+      inset 3px 3px 5px rgba(209, 174, 0, 1);
+    content: '';
+    left: calc(100% - 47.5px);
     right: 7.5px;
   }
+  //배경(Light mode)
   label {
     display: block;
     position: relative;
     width: 120px;
     height: 60px;
     font-weight: 700;
-    border: 5px solid #fff;
-    background: rgb(0, 163, 210, 0.7);
+
+    background: rgba(128, 227, 255, 1);
     border-radius: 60px;
-    transition: background 0.4s;
+    box-shadow: inset -3px -3px 7px rgba(41, 209, 255, 1),
+      inset 3px 3px 5px rgba(0, 172, 219, 0.5);
+    transition: background 0.7s;
   }
+  //공(Light mode)
   label:after {
     display: flex;
     justify-content: center;
@@ -57,27 +64,47 @@ const navToggle = css`
     position: absolute;
     left: 7.5px;
     top: 50%;
-    width: 45px;
-    height: 45px;
+    width: 40px;
+    height: 40px;
     border-radius: 100%;
+    font-size: 0.8rem;
     background: rgb(255, 252, 187);
     transform: translateY(-50%);
-    box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.2);
-    transition: all 0.4s;
+    box-shadow: inset -3px -3px 7px rgba(255, 249, 128, 1),
+      inset 3px 3px 5px rgba(224, 213, 0, 1);
+    transition: all 0.7s;
   }
   label span {
     display: none;
+  }
+
+  input:checked + label div {
+    transform: translateX(71.5px) translateY(12.5px);
+    margin: 0;
+    position: absolute;
+    width: 31px;
+    height: 31px;
+    z-index: 1;
+    border-radius: 100%;
+    background: rgba(0, 51, 102, 1);
   }
 `;
 
 const NavToggle = () => {
   return (
-    <div css={navToggle}>
-      <input type="checkbox" id="chk1" />
-      <label htmlFor="chk1">
-        <span>''</span>
-      </label>
-    </div>
+    <>
+      <div css={navToggle}>
+        <input type="checkbox" id="chk1" />
+
+        <label htmlFor="chk1">
+          <span>''</span>
+
+          <div>
+            <span>''</span>
+          </div>
+        </label>
+      </div>
+    </>
   );
 };
 
