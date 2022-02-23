0b60e2d994981d519ce4059ecbe9d49a32a13219
aosjehdgus <kdhsea@gmail.com>
2022-02-23 17:17:08 +0900

    Apply prop types to nav link component

--- src/components/nav/horizontal-nav/NavLink.js
+++ src/components/nav/horizontal-nav/NavLink.js
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import React from 'react';
 
 const NavLink = ({ item }) => {
   return <Link to={item.path}>{item.title}</Link>;
+};
+
+NavLink.propTypes = {
+  item: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }),
+};
+
+NavLink.defaultProps = {
+  item: {},
 };
 
 export default NavLink;
