86be4d5b77fc5756b02f08ff494b6f396ea85eb1
aosjehdgus <kdhsea@gmail.com>
2022-02-28 22:29:29 +0900

    Add mock api test

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import useSWR from 'swr';
+import { MOCK_URL } from '../configs/AppConfig';
 
 const Profile = () => {
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data, error } = useSWR(`${MOCK_URL}/api/v1/lonnie/profile`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
     <div>
       <h1>Profile</h1>
-      <p>Profile 페이지</p>
+      <p>{data.name}</p>
     </div>
   );
 };
