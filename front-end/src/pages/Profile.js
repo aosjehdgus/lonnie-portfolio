efefd3ba6e30bcf7aa30c611005bbf7be8c104a2
aosjehdgus <kdhsea@gmail.com>
2022-03-01 23:43:48 +0900

    Clean up profile page

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import useSWR from 'swr';
-import { MOCK_URL } from '../configs/AppConfig';
-import Spinner from '../components/common/controls/spinner';
 
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+import About from '../components/profile/About';
+import Project from '../components/profile/Project';
+import Skill from '../components/profile/Skill';
 
 const Profile = () => {
-  const { data, error } = useSWR(`${MOCK_URL}/api/v1/lonnie/profile`, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <Spinner />;
-
-  const { name, university, github, blog } = data;
-
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>{name}</p>
-      <p>{university.name}</p>
-      <p>
-        {university.grade}/{university.totalGrade}
-      </p>
-      <p>{github}</p>
-      <p>{blog}</p>
-    </div>
+    <>
+      <About />
+      <Skill />
+      <Project />
+    </>
   );
 };
 
