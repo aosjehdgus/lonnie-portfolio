45c77b62aa2a24163abab08fc82cbd1c97d48d7a
aosjehdgus <kdhsea@gmail.com>
2022-03-01 11:00:40 +0900

    Add spinner for loading state

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import useSWR from 'swr';
 import { MOCK_URL } from '../configs/AppConfig';
+import Spinner from '../components/spinner';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 const Profile = () => {
-  const fetcher = (...args) => fetch(...args).then(res => res.json());
-
   const { data, error } = useSWR(`${MOCK_URL}/api/v1/lonnie/profile`, fetcher);
 
   if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (!data) return <Spinner />;
 
-  console.log(data);
+  const { name, university, github, blog } = data;
 
   return (
     <div>
       <h1>Profile</h1>
-      <p>{data.name}</p>
+      <p>{name}</p>
+      <p>{university.name}</p>
+      <p>
+        {university.grade}/{university.totalGrade}
+      </p>
+      <p>{github}</p>
+      <p>{blog}</p>
     </div>
   );
 };
