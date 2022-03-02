0f00e5eb5e9f7ae206f40463859fcb896cbfb110
aosjehdgus <kdhsea@gmail.com>
2022-03-02 22:46:49 +0900

    Change use profile hook return

--- front-end/src/swr/useProfile.js
+++ front-end/src/swr/useProfile.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import useSWR from 'swr';
-import Spinner from '../components/common/controls/spinner';
+
 import { MOCK_URL } from '../configs/AppConfig';
 
 export default () => {
   const { data, error } = useSWR(`${MOCK_URL}/api/v1/lonnie/profile`);
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <Spinner />;
-
-  return data;
+  return {
+    profile: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 };
