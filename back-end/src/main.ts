df5b54cb2c7a29943f567582563fbae434e34b9e
aosjehdgus <kdhsea@gmail.com>
2022-03-04 17:37:06 +0900

    Set swagger ui

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // api 문서 이름, 부가 설명, 버전 설정
+  const config = new DocumentBuilder()
+    .setTitle('Profile example')
+    .setDescription('The profile API description')
+    .setVersion('1.0')
+    .build();
+  // 도메인/swagger로 api 문서 접근 가능
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
