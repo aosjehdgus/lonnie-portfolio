a181e961b866a29fb9acabd518e662a659e399d7
aosjehdgus <kdhsea@gmail.com>
2022-03-07 14:47:23 +0900

    Add archive contents

--- front-end/src/contents/profile/ArchiveContent.js
+++ front-end/src/contents/profile/ArchiveContent.js
@@ -1,19 +1,11 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import moment from 'moment';
-import {
-  FaUser,
-  FaUniversity,
-  FaBirthdayCake,
-  FaGithub,
-  FaClipboardList,
-} from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
 
+import ARCHIVE from '../../constants/Archive';
 import useProfile from '../../swr/useProfile';
 import Spinner from '../../components/common/Spinner';
 
-const aboutContentContainer = css`
+const archiveContentContainer = css`
   display: flex;
   justify-content: space-around;
   width: 70%;
@@ -21,44 +13,65 @@ const aboutContentContainer = css`
   border-radius: 1rem;
   padding: 0.5rem;
   position: relative;
-  section {
-    display: flex;
-    padding: 0.5rem;
-    flex-direction: column;
-  }
 `;
 
 const contentWrapper = css`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
-  align-items: center;
   padding: 0.5rem;
   border-radius: 12px;
-  background: rgba(255, 255, 255, 1);
-  div {
+  background: #f8f9fa;
+  text-decoration: none;
+  width: 70%;
+  margin: 1rem;
+  &:hover {
+    transform: translateY(-20px);
+    article {
+      p {
+        text-decoration: underline;
+      }
+    }
+  }
+  header {
     display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
-    padding: 0.5rem 0.5rem 0.5rem 1rem;
+    padding: 1rem;
     h3 {
       color: rgba(0, 0, 0, 0.7);
-      font-size: 1.2rem;
+      font-size: 1.6rem;
+      margin-left: 1rem;
+      font-weight: 700;
+    }
+    img {
+      width: 2.5rem;
+    }
+  }
+  article {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 3;
+    padding: 1rem;
+    p {
+      font-size: 0.9rem;
+      color: #99ccff;
+      padding: 0.5rem;
+      margin-bottom: 1rem;
     }
     span {
-      font-size: 0.9rem;
+      font-size: 0.95rem;
       color: rgba(0, 0, 0, 0.7);
-      margin-top: 0.2rem;
+      padding: 0.5rem;
+      font-weight: 500;
     }
-    a {
-      font-size: 0.9rem;
-      color: rgba(0, 0, 0, 0.7);
-      text-decoration: none;
-      &:hover {
-        font-size: 1.003rem;
-        color: rgba(0, 0, 0, 1);
+    ul {
+      list-style-type: circle;
+      margin-top: 1rem;
+      li {
+        font-size: 0.9rem;
+        padding: 0.5rem;
+        color: rgba(0, 0, 0, 0.7);
       }
     }
   }
@@ -66,29 +79,36 @@ const contentWrapper = css`
 
 const ArchiveContent = () => {
   const { profile, isLoading } = useProfile();
-  // const { name, birth, university, email, github, blog } = profile || {};
-
-  // const birthday = moment(birth).format('YYYY.MM.DD');
 
   return isLoading ? (
     <Spinner />
   ) : (
-    <div css={aboutContentContainer}>
-      <section css={contentWrapper}>
-        <FaGithub size={45} color="rgba(0, 0, 0, 0.7)" />
-        <div>
-          <h3>깃허브</h3>
-          <span>github.com/aosjehdgus</span>
-        </div>
-      </section>
-
-      <section css={contentWrapper}>
-        <FaClipboardList size={45} color="rgba(0, 0, 0, 0.7)" />
-        <div>
-          <h3>블로그</h3>
-          <span>aosjehdgus.tistory.com</span>
-        </div>
-      </section>
+    <div css={archiveContentContainer}>
+      {ARCHIVE.map(({ icon, title, link, description, contents }) => {
+        return (
+          <a
+            target="_blank"
+            href={`https://${link}`}
+            css={contentWrapper}
+            rel="noreferrer"
+            key={title}
+          >
+            <header>
+              {icon}
+              <h3>{title}</h3>
+            </header>
+            <article>
+              <p>{link}</p>
+              <span>{description}</span>
+              <ul>
+                {contents.map(content => {
+                  return <li key={content}>{content}</li>;
+                })}
+              </ul>
+            </article>
+          </a>
+        );
+      })}
     </div>
   );
 };
