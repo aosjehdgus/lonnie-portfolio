04c6cb93c504bbde9d9df1c3f52f3fde2b31ab0d
aosjehdgus <kdhsea@gmail.com>
2022-03-14 23:21:11 +0900

    Add useProject hook

--- front-end/src/contents/profile/ProjectContent.js
+++ front-end/src/contents/profile/ProjectContent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PROJECT from '../../constants/Project';
+import useProject from '../../hooks/useProject';
 
 import Slider from '../../components/carousel/Slider';
 
 const ProjectContent = () => {
+  const { project } = useProject();
   return <Slider project={PROJECT} />;
 };
 
