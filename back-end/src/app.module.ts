210dbfc3306404dc7abb0855ef1fee9119f1e8a2
aosjehdgus <kdhsea@gmail.com>
2022-03-15 11:20:54 +0900

    Connect typeorm to nestjs

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), ProjectModule],
 })
 export class AppModule {}
