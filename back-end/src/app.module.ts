019f8ed5bebd8925e7e1de1a5f07ef7da7f7322f
aosjehdgus <kdhsea@gmail.com>
2022-03-23 12:40:17 +0900

    Apply responsive web

--- back-end/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
-import { ProjectModule } from './project/project.module';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), ProjectModule],
-})
-export class AppModule {}
--- front-end/src/configs/AppConfig.js
+++
@@ -1,5 +0,0 @@
-export const MOCK_URL =
-  'https://a8b365cd-3526-4d0e-bdb3-a747b60465b8.mock.pstmn.io';
-
-export const HOME_PATH = '/';
-export const PROFILE_PATH = '/profile';
