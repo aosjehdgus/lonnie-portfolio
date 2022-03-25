38be4f2efb9ba0ae35e99895c0f9ca7d7b0e34c6
aosjehdgus <kdhsea@gmail.com>
2022-03-25 16:54:19 +0900

    Edit career contents

--- front-end/src/constants/Career.js
+++ front-end/src/constants/Career.js
@@ -1,61 +1,76 @@
-import React from 'react';
-import { FcBriefcase } from '@react-icons/all-files/fc/FcBriefcase';
-import media from '../../asset/media.svg';
+export const PERIOD_CAREER = [
+  { key: 1, year: '2022 - 2021' },
+  { key: 2, year: '2020 - 2013' },
+];
 
-const CAREER = [
+export const CAREER = [
   {
-    key: 'summary',
-    icon: <FcBriefcase size={50} />,
-    title: 'Summary',
-    description: `학부 시절부터 지금까지의 활동을 담고 있습니다.`,
-    contents: [
-      { period: '2013.01 - 2013.12', content: '2대 학생회 과대표' },
-      { period: '2018.01 - 2018.12', content: '7대 학생회 정학생회장' },
-      { period: '2019.01 - 2019.12', content: '8대 학생회 과대표' },
+    key: 1,
+    year: '2022 - 2021',
+    content: [
       {
-        period: '2020.05 - 2020.11',
-        content: '한국품질재단 AI 개발자 양성 과정 수료',
-      },
-      { period: '2021.04 - 2021.11', content: '긱웍스 에아이이' },
-      {
-        period: '2021.11 - 재직중',
-        content: '(주)미디어 코퍼스로 소속 이전',
-      },
-    ],
-  },
-  {
-    key: 'media',
-    icon: <img alt="mediaCorpus" src={media} />,
-    working: '2021.04 - 재직중',
-    description: `미디어 코퍼스는 '인공지능 말뭉치' 구축하는 업체입니다. 저는 삼성전자, 국립국어원의 데이터 구축을 위한 Tool의 프론트 엔드 개발 및 관리를 담당하고 있습니다.`,
-    contents: [
-      {
-        period: '2021.05 - 2021.09',
-        role: '프론트 엔드 개발 인력',
-        project: '말뭉치 언어의 사회적 인식 조사 분류 사업',
-        developments: [
-          '로그인 페이지 개발',
-          '비밀번호 변경 페이지 개발',
-          '말뭉치를 라벨링 하기 위한 컨텍스트 메뉴 개발',
-          '작업 현황 및 파일 진행 상황 점검을 위한 관리 페이지 개발',
-          '작업 완료된 파일의 검수를 위한 페이지 개발',
-        ],
-      },
-      {
-        period: '2021.11 - 2022.04',
-        role: '프론트 엔드 개발 연구원',
-        project: '비윤리적 표현 말뭉치 연구 분석 및 시범 구축 사업',
-        developments: [
+        period: '2021년 11월 - 2022년 현재',
+        description:
+          '국립 국어원 주관 비윤리적 표현 말뭉치 연구 분석 및 시범 구축 사업',
+        role: '프론트 엔드 개발자로 참여',
+        detail: [
           '로그인 페이지 개발',
           '회원 가입 페이지 개발',
           '회원 정보 인증 페이지 개발',
           '비윤리적 표현 항목별 평가 페이지 개발',
-          '작업 현황 점검 페이지 개발',
+          '작업자별 진행률 확인 페이지 개발',
           '공지사항 페이지 개발',
         ],
+      },
+      {
+        period: '2021년 11월',
+        description: '(주) 미디어 코퍼스로 소속 변경',
+      },
+      {
+        period: '2021년 5월 - 2021년 9월',
+        description:
+          '국립 국어원 주관 말뭉치 언어의 사회적 인식 조사 분류 사업',
+        role: '프론트 엔드 개발자로 참여',
+        detail: [
+          '로그인 페이지 개발',
+          '비밀번호 변경 페이지 개발',
+          '말뭉치 라벨링 컨텍스트 메뉴 개발',
+          '작업자별 진행률 확인 페이지 개발',
+          '파일별 완료율 확인 페이지 게발',
+          '완료 파일 검수 페이지 개발',
+        ],
+      },
+      { period: '2021년 4월', description: '긱웍스 에아이이 입사' },
+    ],
+  },
+  {
+    key: 2,
+    year: '2020 - 2013',
+    content: [
+      {
+        period: '2020년 5월 - 2020년 11월',
+        description: '한국품질재단 AI 개발자 양성 과정 수료',
+        role: '',
+        detail: [],
+      },
+      {
+        period: '2019년 1월 - 2019년 12월',
+        description: '나노융합공학과 8대 학생회 과대표',
+        role: '',
+        detail: [],
+      },
+      {
+        period: '2018년 1월 - 2018년 12월',
+        description: '나노융합공학과 9대 학생회 정학생회장',
+        role: '',
+        detail: [],
+      },
+      {
+        period: '2013년 1월 - 2013년 12월',
+        description: '나노융합공학과 2대 학생회 과대표',
+        role: '',
+        detail: [],
       },
     ],
   },
 ];
-
-export default CAREER;
