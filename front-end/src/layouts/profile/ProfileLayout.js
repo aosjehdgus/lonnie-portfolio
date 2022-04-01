c312c7d108dd81a0f27dd1d89f18486eb9dfdec1
aosjehdgus <kdhsea@gmail.com>
2022-04-01 16:38:06 +0900

    Divide profile title

--- front-end/src/layouts/profile/ProfileLayout.js
+++ front-end/src/layouts/profile/ProfileLayout.js
@@ -1,10 +1,8 @@
 /* eslint-disable consistent-return */
-/* eslint-disable react/prop-types */
-/* eslint-disable no-nested-ternary */
 /** @jsx jsx */
 import React, { useState, useEffect, useRef } from 'react';
 import { css, jsx, keyframes } from '@emotion/react';
-import { mq } from '../../constants/MediaQuery';
+import ProfileTitle from '../../components/common/main/ProfileTitle';
 
 const fadeIn = keyframes`
   from {
@@ -24,27 +22,6 @@ const profileContainer = css`
   div {
     animation: ${fadeIn} 1s ease-in-out;
   }
-`;
-const profileContentTitle = prop => css`
-  font-size: 1.5rem;
-  padding: 1rem 0.7rem 1rem 0.7rem;
-  margin-bottom: 2rem;
-  border-radius: 1rem;
-  background: ${prop === 'About'
-    ? 'rgba(255, 227, 115)'
-    : prop === 'Archive'
-    ? 'rgba(0, 0, 0, 0.3)'
-    : prop === 'Skill'
-    ? 'rgba(255, 227, 115)'
-    : prop === 'Activity'
-    ? 'rgba(0, 0, 0, 0.3)'
-    : prop === 'Project'
-    ? 'rgba(255, 227, 115) '
-    : ''};
-  color: white;
-  box-shadow: inset -3px -3px 5px rgba(0, 0, 0, 0.2),
-    inset 3px 3px 5px rgba(0, 0, 0, 0.2);
-  position: relative;
 `;
 
 const ProfileLayout = ({ title, content }) => {
@@ -71,16 +48,7 @@ const ProfileLayout = ({ title, content }) => {
     <div css={profileContainer} ref={scrollRef}>
       {showElement && (
         <>
-          <h1
-            css={css`
-              ${profileContentTitle(title)}
-              ${mq[2]} {
-                font-size: 2rem;
-              }
-            `}
-          >
-            {title}
-          </h1>
+          <ProfileTitle title={title} />
           {content}
         </>
       )}
