1e9e7834fba4d7d79deb11067156a8b3b444462e
aosjehdgus <kdhsea@gmail.com>
2022-04-06 13:47:50 +0900

    Edit about contstants

--- front-end/src/constants/About.js
+++ front-end/src/constants/About.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { FaGithub } from '@react-icons/all-files/fa/FaGithub';
-// import { FcTemplate } from '@react-icons/all-files/fc/FcTemplate';
 import { FcBusinessContact } from '@react-icons/all-files/fc/FcBusinessContact';
 import { FcPlanner } from '@react-icons/all-files/fc/FcPlanner';
 import { FcMindMap } from '@react-icons/all-files/fc/FcMindMap';
@@ -27,16 +25,6 @@ const ABOUT = [
     name: '이메일',
     content: 'kdhsea@gmail.com',
   },
-  // {
-  //   icon: <FaGithub size={50} color="rgba(0, 0, 0, 0.7)" />,
-  //   name: '깃허브',
-  //   content: 'github.com/aosjehdgus',
-  // },
-  // {
-  //   icon: <FcTemplate size={50} />,
-  //   name: '블로그',
-  //   content: 'aosjehdgus.tistory.com',
-  // },
 ];
 
 export default ABOUT;
