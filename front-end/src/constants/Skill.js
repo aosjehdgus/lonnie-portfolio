0142e26e3e512191420e427c829b1e6214cf75e2
aosjehdgus <kdhsea@gmail.com>
2022-04-20 13:05:55 +0900

    Add javascript skill and remove aws

--- front-end/src/constants/Skill.js
+++ front-end/src/constants/Skill.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import JavascriptLogo from '../../asset/javascript.svg';
 import ReactLogo from '../../asset/react.svg';
 import ReduxLogo from '../../asset/redux.svg';
 import ReduxSagaLogo from '../../asset/redux-saga.svg';
 import WebpackLogo from '../../asset/webpack.svg';
-// import NestJsLogo from '../../asset/nestjs.svg';
 import PostmanLogo from '../../asset/postman.svg';
-import AWSLogo from '../../asset/aws.svg';
-// import BitbucketLogo from '../../asset/bitbucket.svg';
 import GitLogo from '../../asset/git.svg';
-// import GithubLogo from '../../asset/github.svg';
-import MongoDBLogo from '../../asset/mongodb.svg';
 import NodeJsLogo from '../../asset/nodejs.svg';
-// import SourceTreeLogo from '../../asset/sourcetree.svg';
 import BabelLogo from '../../asset/babel.svg';
+// import NestJsLogo from '../../asset/nestjs.svg';
+// import AWSLogo from '../../asset/aws.svg';
+// import BitbucketLogo from '../../asset/bitbucket.svg';
+// import GithubLogo from '../../asset/github.svg';
+// import MongoDBLogo from '../../asset/mongodb.svg';
+// import SourceTreeLogo from '../../asset/sourcetree.svg';
 
 const FRONT_SKILL = [
+  {
+    name: 'Javascript',
+    description: '',
+    proficiency: 62,
+    icon: <img alt="javascript" src={JavascriptLogo} />,
+  },
   {
     name: 'React',
     description: '',
@@ -25,19 +32,19 @@ const FRONT_SKILL = [
     name: 'Redux',
     description: '',
     proficiency: 70,
-    icon: <img alt="react" src={ReduxLogo} />,
+    icon: <img alt="redux" src={ReduxLogo} />,
   },
   {
     name: 'Redux-saga',
     description: '',
     proficiency: 67,
-    icon: <img alt="react" src={ReduxSagaLogo} />,
+    icon: <img alt="redux-saga" src={ReduxSagaLogo} />,
   },
   {
     name: 'Webpack',
     description: '',
     proficiency: 57,
-    icon: <img alt="react" src={WebpackLogo} />,
+    icon: <img alt="webpack" src={WebpackLogo} />,
   },
   {
     name: 'Babel',
@@ -52,7 +59,7 @@ const BACK_SKILL = [
     name: 'Node.js',
     description: '',
     proficiency: 51,
-    icon: <img alt="react" src={NodeJsLogo} />,
+    icon: <img alt="node" src={NodeJsLogo} />,
   },
   // {
   //   name: 'MongoDB',
@@ -70,19 +77,19 @@ const BACK_SKILL = [
     name: 'Postman',
     description: 'Postman 재밌다',
     proficiency: 62,
-    icon: <img alt="react" src={PostmanLogo} />,
+    icon: <img alt="postman" src={PostmanLogo} />,
   },
 ];
 const CLOUD_SKILL = [
-  {
-    name: 'AWS',
-    proficiency: 51,
-    icon: <img alt="react" src={AWSLogo} />,
-  },
+  // {
+  //   name: 'AWS',
+  //   proficiency: 51,
+  //   icon: <img alt="react" src={AWSLogo} />,
+  // },
   {
     name: 'Git',
     proficiency: 52,
-    icon: <img alt="react" src={GitLogo} />,
+    icon: <img alt="git" src={GitLogo} />,
   },
   // {
   //   name: 'Github',
